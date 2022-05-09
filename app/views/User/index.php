11089c1c1b0e219c316d1b6690d9ff130579b0d3
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-05-09 12:04:20 -0400

    Css updates

--- app/views/User/index.php
+++ app/views/User/index.php
@@ -1,35 +1,44 @@
 <html>
     <head>
         <!-- CSS only -->
+        
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-            <!-- JavaScript Bundle with Popper -->
+        <link rel="stylesheet" type="text/css" href="/app/public/css/update.css">   
+        <!-- JavaScript Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <title><?= $data->first_name . " " . $data->last_name?></title>
     </head>
     <body> 
-        <div class='container'>
-            <?php
+        <?php
                 $this->view('subviews/navigation');
             ?>
+        <div class='containers'>
+            
             <div style = 'text-align:center;'>
-                <h1>Your Profile</h1>
+                <h1 id="title">Your Profile</h1>
                 <form method='post' action=''>
-                    <label class='form-label'>First name:<input disabled type='text' name='first_name' class='form-control' value = '<?= $data->first_name?>' /></label>
-                    <label class='form-label'>Last name:<input disabled type='text' name='last_name' class='form-control'value = '<?= $data->last_name?>'  /></label> <br>
-                    <label class='form-label'>Email:<input disabled type='email' name='email' class='form-control' value = '<?= $data->email?>' /></label><br>
+                <div id="name">
+                First name:</div><label class='form-labels'><input  type='text' name='first_name' class='form-control' value = '<?= $data->first_name?>' /></label>
+                <div id="Contact">
+                Last name:</div><label class='form-labels'><input  type='text' name='last_name' class='form-control'value = '<?= $data->last_name?>'  /></label> <br>
+                <div id="email">
+                Email:</div><label class='form-labels'><input  type='email' name='email' class='form-control' value = '<?= $data->email?>' /></label><br>
                     
                 </form> 
+                <div class="">
                 <?php
 
                     if (isset($_SESSION['user_id']) && $_SESSION['user_id'] == $data->user_id) {
-                        echo "<a class=\"btn btn-primary\" href='/User/update/$data->user_id' class='m-2' >". "Update User" ." </a>";
+                        echo "<a class=\"update-link\" href='/User/update/$data->user_id' class='update-link' >". "Update User" ." </a>";
 
                         if (isset($_SESSION['role']) && $_SESSION['role'] != '1') 
-                            echo "&ensp;<a class=\"btn btn-primary\" href='/User/delete/$data->user_id' class='m-2' >". "Delete User" ."</a>";
+                            echo "&ensp;<a class=\"delete-link\" href='/User/delete/$data->user_id' class='delete-link' >". "Delete User" ."</a>";
                     }
+                   
             echo "</div>"; 
-                ?>
             
+                ?>
+            </div>
         </div>
     </body>
 </html>
--- app/views/User/register.php
+++ app/views/User/register.php
@@ -10,19 +10,21 @@
 <h1 id="title"><?= _("Register Here!") ?></h1>
 <form method='post' action=''>
 <div id="name">
-<?= _("Username") ?></div><label class='form-label'><input type='text' name='username' class='form-control' required/></label><br>
-<div id="password">
-<?= _("Password") ?></div><label class='form-label'><input type='password' name='password' class='form-control' required/></label><br>
-<div id="password">
-<?= _("Password confirmation") ?></div><label class='form-label'><input type='password' name='password_confirm' class='form-control' required/></label><br>
-<div id="email">
-<?= _("Email") ?></div><label class='form-label'><input type='email' name='email' class='form-control'required /></label><br>
+First Name:</div><label class='form-label'><input type='text' name='username' class='form-control' required/></label><br>
 <div id="Contact">
-<?= _("Contact") ?></div><label class='form-label'><input type='tel' name='contact' class='form-control' required/></label><br>
+Last Name:</div><label class='form-label'><input type='tel' name='contact' class='form-control' required/></label><br>
+<div id="password">
+<div id="email">
+Email:</div><label class='form-label'><input type='email' name='email' class='form-control'required /></label><br>
+Password:</div><label class='form-label'><input type='password' name='password' class='form-control' required/></label><br>
+<div id="password">
+Password Confirmation:</div><label class='form-label'><input type='password' name='password_confirm' class='form-control' required/></label><br>
+
+
 	<input type="submit" name='action' value='Register!' class='form-control' />
 </form>
 <div class='signup-link'>
-<?= _("Already have an account? ") ?> <a href="/User/login"><?= _("Login here!") ?></a> <br>
+Already have an account?<a href="/User/login">Login</a> <br>
 <?php
 				if ($data)
 					echo "<div class='alert alert-danger' role='alert'> $data</div>";
