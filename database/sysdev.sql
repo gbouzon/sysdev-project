96f5769caf799e963c5ed98c79803395895a33cf
gbouzon <ga@bouzon.com.br>
2022-05-09 07:14:08 -0400

    updated database

--- database/sysdev.sql
+++ database/sysdev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 03, 2022 at 03:36 AM
+-- Generation Time: May 09, 2022 at 01:13 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.0.13
 
@@ -24,30 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `customer`
+-- Table structure for table `address`
 --
 
-CREATE TABLE `customer` (
-  `customer_id` int(11) NOT NULL,
+CREATE TABLE `address` (
+  `address_id` int(11) NOT NULL,
   `street_name` varchar(30) DEFAULT NULL,
   `street_number` varchar(10) DEFAULT NULL,
   `city` varchar(30) DEFAULT NULL,
   `province` varchar(30) DEFAULT NULL,
-  `postal_code` varchar(6) DEFAULT NULL,
-  `user_id` int(11) NOT NULL
+  `country` varchar(30) DEFAULT NULL,
+  `postal_code` varchar(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `employee`
+-- Dumping data for table `address`
 --
 
-CREATE TABLE `employee` (
-  `employee_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `role` tinyint(4) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `address` (`address_id`, `street_name`, `street_number`, `city`, `province`, `country`, `postal_code`) VALUES
+(1, 'Your M', '120', 'montreal', 'QC', 'Canada', 'H2E1T2');
 
 -- --------------------------------------------------------
 
@@ -57,11 +52,23 @@ CREATE TABLE `employee` (
 
 CREATE TABLE `order` (
   `order_id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `total` double DEFAULT 0,
   `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
   `order_status` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0 = in cart, 1 = pending, 2 = accepted, 3 = shipped, 4 = delivered/closed'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`order_id`, `user_id`, `total`, `createdAt`, `order_status`) VALUES
+(1, 3, 0, '2022-05-09 04:09:14', 0),
+(2, 1, 0, '2022-05-09 08:59:45', 0),
+(3, 1, 0, '2022-05-09 09:00:46', 0),
+(4, 1, 0, '2022-05-09 09:03:21', 0),
+(5, 1, 0, '2022-05-09 09:21:48', 0),
+(6, 1, 0, '2022-05-09 10:32:18', 0);
 
 -- --------------------------------------------------------
 
@@ -86,12 +93,20 @@ CREATE TABLE `order_detail` (
 
 CREATE TABLE `product` (
   `product_id` int(11) NOT NULL,
-  `product_image` varchar(50) NOT NULL DEFAULT 'blank,png',
+  `product_image` varchar(50) NOT NULL DEFAULT 'blank.png',
   `product_name` varchar(30) NOT NULL,
-  `description` varchar(100) NOT NULL,
+  `description` varchar(500) NOT NULL,
   `price` double NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_image`, `product_name`, `description`, `price`, `quantity`) VALUES
+(1, 'beard_oil.png', 'Beard Oil', 'Beard oil is a conditioner used to moisturize and soften beard hair. It\'s also effective for moisturizing the skin beneath your beard. People use beard oil to keep their beards looking fuller, softer, and tamer. It\'s also sometimes used to promote beard growth.', 40, 10),
+(3, 'beard_balm.jpg', 'Beard Balm', 'A beard balm works as a leave-in conditioner which will moisturize, condition, style and soften your beard. All of these ingredients are combined to help promote proper beard growth and keep your beard healthy and smelling its best.', 25, 2);
 
 -- --------------------------------------------------------
 
@@ -104,39 +119,42 @@ CREATE TABLE `user` (
   `email` varchar(254) NOT NULL,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
-  `password_hash` varchar(63) NOT NULL
+  `password_hash` varchar(63) NOT NULL,
+  `address_id` int(11) DEFAULT NULL,
+  `role` tinyint(4) NOT NULL DEFAULT 0 COMMENT 'o is for customer, 1 is for admin, 2 is for supervisor and 3 is for employee'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `email`, `first_name`, `last_name`, `password_hash`, `address_id`, `role`) VALUES
+(1, 'admin@gmail.com', 'ADMIN', 'ADMIN', '$2y$10$LFrXr6u9SDTpV05WgcpvSulwKzuCRLCu239MzPdE3rL5uaxDgEwti', NULL, 1),
+(3, 'test@gmail.com', 'First', 'Last', '$2y$10$zY3WZkwGlV9mJCLYyafePeY/lox6cGRTDOlMkC3VZqXSpruNXsJuy', 1, 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `customer`
+-- Indexes for table `address`
 --
-ALTER TABLE `customer`
-  ADD PRIMARY KEY (`customer_id`),
-  ADD KEY `customer_user_fk` (`user_id`);
-
---
--- Indexes for table `employee`
---
-ALTER TABLE `employee`
-  ADD PRIMARY KEY (`employee_id`);
+ALTER TABLE `address`
+  ADD PRIMARY KEY (`address_id`);
 
 --
 -- Indexes for table `order`
 --
 ALTER TABLE `order`
   ADD PRIMARY KEY (`order_id`),
-  ADD KEY `order_customer_fk` (`customer_id`);
+  ADD KEY `order_customer_fk` (`user_id`);
 
 --
 -- Indexes for table `order_detail`
 --
 ALTER TABLE `order_detail`
   ADD PRIMARY KEY (`order_detail_id`),
-  ADD UNIQUE KEY `order_id` (`order_id`, `product_id`),
+  ADD UNIQUE KEY `order_id` (`order_id`,`product_id`),
   ADD KEY `orderdetail_product_fk` (`product_id`);
 
 --
@@ -150,70 +168,65 @@ ALTER TABLE `product`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `user_email_unique` (`email`);
+  ADD UNIQUE KEY `user_email_unique` (`email`),
+  ADD KEY `user_address_fk` (`address_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `customer`
+-- AUTO_INCREMENT for table `address`
 --
-ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `employee`
---
-ALTER TABLE `employee`
-  MODIFY `employee_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `address`
+  MODIFY `address_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `order`
 --
 ALTER TABLE `order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `order_detail`
 --
 ALTER TABLE `order_detail`
-  MODIFY `order_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `order_detail_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=141;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `customer`
---
-ALTER TABLE `customer`
-  ADD CONSTRAINT `customer_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
-
---
 -- Constraints for table `order`
 --
 ALTER TABLE `order`
-  ADD CONSTRAINT `order_customer_fk` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `order_customer_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `order_detail`
 --
 ALTER TABLE `order_detail`
-  ADD CONSTRAINT `orderdetail_product_fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `orderdetail_order_fk` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `orderdetail_order_fk` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `orderdetail_product_fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_address_fk` FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
