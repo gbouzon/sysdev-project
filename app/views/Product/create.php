42d7268ae276639e45e4d7aeb4420b6c66a7dde5
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-05-10 12:24:54 -0400

    admin create UI added

--- app/views/Product/create.php
+++ app/views/Product/create.php
@@ -2,17 +2,20 @@
 	<head>
 		<!-- CSS only -->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-			<!-- JavaScript Bundle with Popper -->
+		<link rel="stylesheet" type="text/css" href="/app/public/css/productcreate.css"> 
+    
+		<!-- JavaScript Bundle with Popper -->
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 			<title>Add Product</title>
 	</head>
 	<body>
-		<div class='container' style='text-align:center;'>
-			<?php
+	<?php
 				$this->view('subviews/navigation');
 			?>
+		<div class='container' style='text-align:center;'>
+			
 
-			<h1>Add Product</h1>
+			<h1 id="title">Add Product</h1>
 		
 			<form method='post' action='' enctype = 'multipart/form-data'>
 				<label class='form-label'>Product name:<input type='text' name='product_name' class='form-control' required/></label> <br>
--- app/views/subviews/products.php
+++ app/views/subviews/products.php
@@ -1,3 +1,10 @@
+<head>
+    <title>Products</title>
+    <link rel="stylesheet" type="text/css" href="/app/public/css/products.css"> 
+    
+        
+</head>
+
 <body style = "background-color: #302c3c;">
     <h2 class = "text-center" style="text-align:center;white-space:pre-wrap;color:white;">Our Products:</h2><br><br>
     <p style="text-align:center;white-space:pre-wrap;color:white;">Feel great, have a fresh scent with sophisticated aroma, and keep your beard healthy.</p> <br> <br>
