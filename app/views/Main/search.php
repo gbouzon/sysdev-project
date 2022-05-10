dcf500d7e7fe9832934f19a81211c55de72824be
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-05-10 13:53:06 -0400

    search bar added

--- app/views/Main/search.php
+++ app/views/Main/search.php
@@ -1,8 +1,10 @@
 <html>
 	<head>
 		<title>Search</title>
+		
 	</head>
 	<body>
+		
 		<form method="post" action = "">
 			<input type="text" name="search">
 			<button type="submit" name="action">Search</button>
--- app/views/Search/index.php
+++ app/views/Search/index.php
@@ -2,11 +2,29 @@
 <html>
 	<head>
 		<title>Search</title>
+		<link rel="stylesheet" type="text/css" href="/app/public/css/searchbar.css"> 
+        <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    />
+    
 	</head>
 	<body>
+
 		<form method="post" action = "" style = "position:absolute;top:10px;right:100px;">
-			<input type="text" name="search">
-			<button type="submit" name="action">Search</button>
+			<div class="search-input-wrapper">
+      			<div class="search-input">	
+					<input type="text" name="search" placeholder="Search" >
+					<i class="fas fa-times"></i>
+				</div>
+			</div>
+			<div class="search-icon">
+					<i class="fas fa-search" name="action"><button type="submit" name="action"></button></i>
+			</div>
+			
 		</form>
+		
+    </div>
+	<script src="/app/public/js/script.js"></script>
 	</body>
 </html>
