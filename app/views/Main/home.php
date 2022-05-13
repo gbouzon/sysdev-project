09f864249628200356622da241a8b546771fbace
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-05-12 21:22:25 -0400

    shop button fixed

--- app/views/Main/home.php
+++ app/views/Main/home.php
@@ -1,7 +1,9 @@
 <html>
     <head>
             <!-- CSS only -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">  
+            <link rel="stylesheet" type="text/css" href="/app/public/css/force.css"> 
+        
             <!-- JavaScript Bundle with Popper -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <style >
@@ -17,6 +19,9 @@
                 $this->view('subviews/navigation');
             ?>
             <br> <h1 class="text-center" style = "color:white;font-family:'Times New Roman', Times, serif; font-size:76px;text-align:center;white-space:pre-wrap;">Health and Growth</h1><br>
+            <div id="button">
             <a class="btn btn-primary btn-lg" href='/Main/products' class='m-2' >Shop Now</a>
+            </div>
+        </div>
     </body>
 </html>
